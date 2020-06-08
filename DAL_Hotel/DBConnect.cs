7c37a1cabf38f6e271a93f7fdeba0a6669151b87
Huy Quynh <huyquynh11to@gmail.com>
2020-06-08 13:21:05 +0700

    add

---
+++ DAL_Hotel/DBConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL_Hotel
+{
+    public class DBConnect
+    {
+        protected SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-B182N8G\MSSQLSERVER01;Initial Catalog=QLKS;Integrated Security=True");
+    }
+}
